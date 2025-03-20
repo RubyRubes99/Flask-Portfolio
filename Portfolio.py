ee96811897108641c32f9aecbad35ad6b6d0aff5
RubinAbraham <rabraham.oak@gmail.com>
2025-03-20 17:53:07 +0200

    Trying to add media to Portfolio

--- Portfolio.py
+++ Portfolio.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for
 
 app = Flask(__name__)
 
@@ -8,20 +8,30 @@ projects = [
         "title": "Computer Vision Piano System",
         "description": "A computer vision-based piano that detects key presses using computer vision.",
         "video_url": "https://github.com/RubyRubes99/Computer-Vision-Based-Piano-System.git",
+        "image_url": url_for('static', filename='CVPiano.png'),
+        "github": "",
+        "status": "Demo Available"
+    },
+    {
+        "title": "Simple Real Time Hand Detector",
+        "description": "A real time hand detector made using computer vision techniques such as template matching and skin detection.",
+        "image_url": url_for('static', filename='FD_FingerDetected.gif'),
         "github": "",
         "status": "Demo Available"
     },
     {
         "title": "WiFi Lamp",
         "description": "An Android app that controls a lamp over WiFi.",
+        "image_url": url_for('static', filename='WifiLampDisplay.jpg'),
         "github": "https://github.com/RubyRubes99/WiFi-Lamp.git",
         "status": "Code Available, No Demo"
     },
     {
         "title": "Speech-to-Text Recognition App",
         "description": "An Android app that transcribes audio into text using offline ML.",
+        "image_url": url_for('static', filename='WifiLampDisplay.jpg'),
         "github": "https://github.com/your-repo",
-        "status": "Currently not available_Demo & Code Available"
+        "status": "Currently not available"
     }
 ]
 
